f80a1c8beadbe635c7d5fa44647d3b97d9462226
Devil Devilson <devilsdevofficial@gmail.com>
2019-07-18 22:28:50 +0300

    functions threadsave versions

--- bin/EventComponent.cpp
+++ bin/EventComponent.cpp
@@ -43,3 +43,49 @@ event EventComponent::fireEvent(const Type &type, const EventData &data) {
   
   return val;
 }
+
+void EventComponent::registerEvent_save(const Type &type, const std::function<event(const Type &, const EventData &)> &func) {
+  std::unique_lock<std::mutex> lock(local_mutex);
+  events[type].push_back(func);
+}
+
+bool EventComponent::hasEvent_save(const Type &type) const {
+  std::unique_lock<std::mutex> lock(local_mutex);
+  auto itr = events.find(type);
+  if (itr == events.end()) return false;
+  
+  return !itr->second.empty();
+}
+
+event EventComponent::fireEvent_save(const Type &type, const EventData &data) {
+  std::unique_lock<std::mutex> lock(local_mutex);
+//   {
+//     std::unique_lock<std::mutex> lock(local_mutex);
+    auto itr = events.find(type);
+    if (itr == events.end()) return failure;
+//   }
+  
+  event val = success;
+  
+//   {
+//     std::unique_lock<std::mutex> lock(local_mutex);
+    
+    for (size_t i = 0; i < itr->second.size(); ++i) {
+      const event ret = itr->second[i](type, data);
+      
+      if ((ret & can_be_deleted) == can_be_deleted) {
+        std::swap(itr->second[i], itr->second.back());
+        itr->second.pop_back();
+        --i;
+      }
+      
+      const uint32_t mask = ~static_cast<uint32_t>(can_be_deleted);
+      const event tmp = static_cast<event>(mask & ret);
+      val = std::max(val, tmp);
+    }
+    
+    if (itr->second.empty()) events.erase(itr);
+//   }
+  
+  return val;
+}
--- bin/EventComponent.h
+++ bin/EventComponent.h
@@ -7,6 +7,8 @@
 
 #include "Event.h"
 
+#include <mutex>
+
 class EventComponent : public yacs::Component {
 public:
   CLASS_TYPE_DECLARE
@@ -20,7 +22,14 @@ public:
   void registerEvent(const Type &type, const std::function<event(const Type &, const EventData &)> &func);
   bool hasEvent(const Type &type) const;
   event fireEvent(const Type &type, const EventData &data);
+  
+  void registerEvent_save(const Type &type, const std::function<event(const Type &, const EventData &)> &func);
+  bool hasEvent_save(const Type &type) const;
+  event fireEvent_save(const Type &type, const EventData &data);
 private:
+  // тут нужно добавить мьютекс 
+  // и добавить безопасные функции которые будут вызывать мьютекс
+  mutable std::mutex local_mutex;
   std::unordered_map<Type, std::vector<std::function<event(const Type &, const EventData &)>>> events;
 };
 
