40a3e3a6a2977b081f8f499ba72968733bc991e2
Devil Devilson <devilsdevofficial@gmail.com>
2019-08-10 19:39:44 +0300

    fix Engine class

--- utils/Engine.h
+++ utils/Engine.h
@@ -2,12 +2,13 @@
 #define ENGINE_H
 
 #include <cstdint>
+#include <cstddef>
 
 class Engine {
 public:
   virtual ~Engine() {}
   
-  virtual void update(const uint64_t &time) = 0;
+  virtual void update(const size_t &time) = 0;
   // должен появиться метод clean для того чтобы подготовиться к следуюшей карте
 };
 
